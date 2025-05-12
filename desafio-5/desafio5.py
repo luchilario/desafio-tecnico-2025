20b4fe03690773051afac295dcd459e5cabc6467
luchilario <152821298+luchilario@users.noreply.github.com>
2025-05-12 15:15:03 -0300

    Update desafio5.py

--- desafio-5/desafio5.py
+++ desafio-5/desafio5.py
@@ -1,1 +1,9 @@
+def inverter_string(s):
+    invertida = ""
+    for i in range(len(s) - 1, -1, -1):
+        invertida += s[i]
+    return invertida
 
+texto = "Luquinha"
+print("Original:", texto)
+print("Invertida:", inverter_string(texto))
