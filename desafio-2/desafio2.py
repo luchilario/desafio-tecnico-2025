8befbe27727a709628eff6924789c0750fce4722
luchilario <152821298+luchilario@users.noreply.github.com>
2025-05-12 15:13:42 -0300

    Update desafio2.py

--- desafio-2/desafio2.py
+++ desafio-2/desafio2.py
@@ -1,1 +1,11 @@
+def is_fibonacci(n):
+    a, b = 0, 1
+    while b < n:
+        a, b = b, a + b
+    return n == b or n == 0
 
+numero = 21  # ou use input("Digite um número: ")
+if is_fibonacci(numero):
+    print(f"O número {numero} pertence à sequência de Fibonacci.")
+else:
+    print(f"O número {numero} NÃO pertence à sequência de Fibonacci.")
